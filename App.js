6c6d2ea9f0dcd54410bce5241138b083093c896b
ZEROwolf <zerowolfhwang@gmail.com>
2019-03-25 15:38:40 +0800

    [MOD]app.js

--- App.js
+++ App.js
@@ -12,20 +12,26 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-
-        android:
+      android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
+
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'hahah'
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{this.state.name}</Text>
       </View>
     );
   }
